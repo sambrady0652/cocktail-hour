1f96c7ffdae4f353a56b7a916af315b5657a2b7f
Sam Brady <sambrady0652@gmail.com>
2020-09-11 13:48:30 -0500

    Ingredients_Route_Fix

--- app/routes/ingredient_routes.py
+++ app/routes/ingredient_routes.py
@@ -3,7 +3,7 @@ import boto3
 from flask import Blueprint, jsonify, request
 import os
 import re
-# This is an operator that works in conjunction with the postgres-dialect ARRAY Datatype
+# This operator works with the SQLAlchemy ARRAY Data Type used in the Model
 from sqlalchemy.sql.operators import ilike_op
 
 # Local Imports
@@ -15,31 +15,29 @@ ingredient_routes = Blueprint('ingredients', __name__)
 # Returns All Ingredients
 @ingredient_routes.route('/')
 def get_options():
-    ingredient_list = Ingredient.query.all()
+    ingredient_list = Ingredient.query.limit(5).all()
     return {'results': [ingredient.to_dict()
                         for ingredient in ingredient_list]}
 
 
-# Finds list of drinks that include the searched ingredient
+# Search By Ingredient Type
 @ingredient_routes.route('/search/results', methods=['POST'])
 def get_results():
-    # This is the term that is selected by the user. It is selected from a provided list of Ingredient Types
+    # Dropdown menu on front-end provides ingredient Types. Retrieve it here
     ingredient_type = request.json.get('searchTerm')
-    # if for some reason it is empty, return empty, so as to avoid 'undefined' error
     if ingredient_type == "":
         return {'results': []}
-    # Create list of Ingredient Names for each ingredient with a type like that of the searched term
-    ingredients_list = [ingredient.to_dict()['name']
-                        for ingredient in Ingredient.query.filter(
-        Ingredient.type.ilike(ingredient_type + '%')).all()]
+    # Find all ingredients of specified type, make a list of their Names
+    ingredients_list = [ingredient.to_dict()['name'] for ingredient in Ingredient.query.filter(
+        Ingredient.type.ilike(ingredient_type + "%")).all()]
 
-    # Find all drinks that include an ingredient of the provided type
+    # For each ingredient in list created above, find drinks that include the ingredient
     drinks_list = []
     for ingredient in ingredients_list:
-        drinks = Drink.query.filter(
-            Drink.ingredients.any(ingredient, operator=ilike_op)).all()
+        drinks = Drink.query.filter(Drink.ingredients.any(
+            ingredient, operator=ilike_op)).all()
         drinks_list.extend(drinks)
 
-    # Return list of all drinks converted to Dictionary
+    # Return list of drinks
     return {'results': [drink.to_dict()
                         for drink in drinks_list]}
