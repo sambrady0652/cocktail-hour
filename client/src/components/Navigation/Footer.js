2ef162a454f27995379596ae8cc105e5cc5ca48a
Sam Brady <sambrady0652@gmail.com>
2020-09-17 13:35:12 -0500

    Footer Restyling

--- client/src/components/Navigation/Footer.js
+++ client/src/components/Navigation/Footer.js
@@ -5,46 +5,50 @@ import { Github, LinkedinOption } from 'grommet-icons';
 
 
 const Foot = () => {
-
   return (
     <Footer
+      align="center"
       pad={{ horizontal: "small" }}
       fill="horizontal"
-      justify="between"
-      background="#362725B3"
+      justify="end"
+      // background="#362725B3"
       style={{
         position: "fixed",
         bottom: "0px"
       }}
     >
-      <Box
-        align="center"
-        direction="row"
-      >
-        <Text size="xsmall">Application by Sam Brady</Text>
-        <Anchor
-          target="_blank"
-          size="xsmall"
-          margin="none"
-          href="https://github.com/sambrady0652"
-          icon={<Github color="#FDCF89" />}>
-        </Anchor>
-        <Anchor
-          target="_blank"
-          size="xsmall"
-          margin="none"
-          href="https://www.linkedin.com/in/sam-brady-0652/"
-          icon={<LinkedinOption color="#FDCF89" />}>
-        </Anchor>
+      <Box direction="row">
+        <Box>
+          <Anchor
+            style={{ padding: "2px" }}
+            plain
+            target="_blank"
+            size="xsmall"
+            margin="none"
+            href="https://github.com/sambrady0652"
+            icon={<Github color="#FDCF89" />}>
+          </Anchor>
+          <Anchor
+            style={{ padding: "2px" }}
+            target="_blank"
+            size="xsmall"
+            margin="none"
+            href="https://www.linkedin.com/in/sam-brady-0652/"
+            icon={<LinkedinOption color="#FDCF89" />}>
+          </Anchor>
+        </Box>
+        <Box align='end' justify="center">
+          <Text size="xsmall" color="#FDCF89">Application by Sam Brady</Text>
+          <Text size="xsmall" color="#FDCF89">
+            Data supplied by <Anchor
+              target="_blank"
+              href="https://www.thecocktaildb.com/api.php"
+              color="#FDCF89"  >
+              The Cocktail DB
+              </Anchor>
+          </Text>
+        </Box>
       </Box>
-      <Text size="xsmall">
-        Data supplied by <Anchor
-          target="_blank"
-          href="https://www.thecocktaildb.com/api.php"
-          color="#FDCF89"  >
-          The Cocktail DB
-        </Anchor>
-      </Text>
     </Footer >
   )
 }
