fd6eb2f97466a7485acdf5f6cd4c43f7f056b16c
Sam Brady <sambrady0652@gmail.com>
2020-09-17 17:38:25 -0500

    footer stylings, updated flask to include favicon

--- client/src/components/Navigation/Footer.js
+++ client/src/components/Navigation/Footer.js
@@ -11,35 +11,36 @@ const Foot = () => {
       pad={{ horizontal: "small" }}
       fill="horizontal"
       justify="end"
-      // background="#362725B3"
       style={{
         position: "fixed",
         bottom: "0px"
       }}
     >
       <Box direction="row">
-        <Box>
-          <Anchor
-            style={{ padding: "2px" }}
-            plain
-            target="_blank"
-            size="xsmall"
-            margin="none"
-            href="https://github.com/sambrady0652"
-            icon={<Github color="#FDCF89" />}>
-          </Anchor>
-          <Anchor
-            style={{ padding: "2px" }}
-            target="_blank"
-            size="xsmall"
-            margin="none"
-            href="https://www.linkedin.com/in/sam-brady-0652/"
-            icon={<LinkedinOption color="#FDCF89" />}>
-          </Anchor>
-        </Box>
         <Box align='end' justify="center">
-          <Text size="xsmall" color="#FDCF89">App by Sam Brady</Text>
-          <Text size="xsmall" color="#FDCF89">
+          <Footer justify="between">
+            <Box direction="row">
+              <Anchor
+                style={{ padding: "2px" }}
+                plain
+                target="_blank"
+                size="xsmall"
+                margin="none"
+                href="https://github.com/sambrady0652"
+                icon={<Github color="#FDCF89" />}>
+              </Anchor>
+              <Anchor
+                style={{ padding: "2px" }}
+                target="_blank"
+                size="xsmall"
+                margin="none"
+                href="https://www.linkedin.com/in/sam-brady-0652/"
+                icon={<LinkedinOption color="#FDCF89" />}>
+              </Anchor>
+            </Box>
+            <Text size="xsmall" color="#FDCF89">App by Sam Brady</Text>
+          </Footer>
+          <Text size="xsmall" color="#FDCF89" margin={{ bottom: "xsmall" }}>
             Data supplied by <Anchor
               target="_blank"
               href="https://www.thecocktaildb.com/api.php"
